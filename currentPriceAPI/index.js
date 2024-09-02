e6885858e756d354dcf48547d748f824b04e39b4
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-02 14:04:46 +0900

    ✨feat: 채권 현재가 api 구현

--- currentPriceAPI/index.js
+++ currentPriceAPI/index.js
@@ -47,3 +47,44 @@ app.post('/api/stock/current-price', async (req, res) => {
 });
 
 
+
+app.post('/api/bond/current-price', async (req, res) => {
+    const { stockId } = req.body; 
+
+    if (!stockId) {
+        return res.status(400).json({ error: "stockId is required" });
+    }
+
+    try {
+        const ACCESS_TOKEN = await getToken();
+        const PATH = "/uapi/domestic-bond/v1/quotations/inquire-price";
+        const URL = `${URL_BASE}/${PATH}`;
+        const authorization = ACCESS_TOKEN.access_token;
+        const token_type = ACCESS_TOKEN.token_type;
+        const headers = {
+            "Content-Type": "application/json",
+            authorization: `${token_type} ${authorization}`,
+            appKey: key,
+            appSecret: secret,
+            tr_id: "FHKBJ773400C0",
+            custtype: "P"
+        };
+
+        const params = {
+            fid_cond_mrkt_div_code: "B",
+            fid_input_iscd: stockId,
+        };
+        const response = await axios.get(URL, { headers, params });
+        return res.json(response.data); 
+    } catch (error) {
+        console.error("Error:", error.message);
+        return res.status(500).json({
+            error_description: 'Failed to fetch current price',
+            error_code: 'SERVER_ERROR'
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`REST API server is listening on port ${port}`);
+});
