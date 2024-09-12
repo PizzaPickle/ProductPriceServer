4d6f0cfda74d06095a6dcc433754de6550d25d83
gaeon <rkdjs1104@ajou.ac.kr>
2024-09-12 17:24:35 +0900

    ✨feat: 업데이트 버전 올립니당

--- RealTimePriceServer/app.js
+++ RealTimePriceServer/app.js
@@ -1,42 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const http = require("http");
 const { processClientsSequentially } = require("./wsHandler");
-const port = 8080;
+const port = 8080; // 안쓰는 포트로 바꿔주세용!
 const server = http.createServer(app);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
 const connections = [];
 wss.on("connection", function connection(ws) {
-    console.log("새로운 WebSocket 클라이언트가 연결되었습니다.");
+  console.log("새로운 WebSocket 클라이언트가 연결되었습니다.");
 
-    ws.on("message", (message) => {
-        try {
-            const data = JSON.parse(message);
-            
-            if (data.type === 'subscribe') {
-                console.log("Received data:", data);
-                const { id } = data;
-                connections.push({ ws, stockId: id });
-                console.log(`종목 ID ${id}에 대한 연결 `);
-                processClientsSequentially(connections, 7000, 8000) 
-            }
-        } catch (error) {
-            console.error("Error parsing message:", error.message);
-        }
-    
-    });
-    ws.on("close", function close() {
-        console.log("WebSocket 연결이 종료되었습니다.");
-        const index = connections.findIndex(connection => connection.ws === ws);
-        if (index !== -1) {
-            connections.splice(index, 1);
-            console.log("연결이 connections 배열에서 제거되었습니다.");
-        }
-    });
+  ws.on("message", (message) => {
+    try {
+      const data = JSON.parse(message);
+
+      if (data.type === "subscribe") {
+        console.log("Received data:", data);
+        const { id } = data;
+        connections.push({ ws, stockId: id });
+        console.log(`종목 ID ${id}에 대한 연결 `);
+        processClientsSequentially(connections, 7000, 8000);
+      }
+    } catch (error) {
+      console.error("Error parsing message:", error.message);
+    }
+  });
+  ws.on("close", function close() {
+    console.log("WebSocket 연결이 종료되었습니다.");
+    const index = connections.findIndex((connection) => connection.ws === ws);
+    if (index !== -1) {
+      connections.splice(index, 1);
+      console.log("연결이 connections 배열에서 제거되었습니다.");
+    }
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
-
--- currentPriceAPI/index.js
+++ currentPriceAPI/index.js
@@ -1,150 +1,141 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const { getToken } = require("./apiUtils");
 const app = express();
-const port = 8081; 
+const port = 8081; // 안쓰는 포트로 바꿔주세용!
 const key = process.env.KEY;
 const secret = process.env.SECRET_KEY;
 app.use(express.json());
 
 const URL_BASE = "https://openapi.koreainvestment.com:9443";
 
-app.post('/api/stock/current-price', async (req, res) => {
-    const { stockId } = req.body; 
+app.post("/currentprice/stock/current-price", async (req, res) => {
+  const { stockId } = req.body;
 
-    if (!stockId) {
-        return res.status(400).json({ error: "stockId is required" });
-    }
+  if (!stockId) {
+    return res.status(400).json({ error: "stockId is required" });
+  }
 
-    try {
-        const ACCESS_TOKEN = await getToken();
-        const PATH = "uapi/domestic-stock/v1/quotations/inquire-price";
-        const URL = `${URL_BASE}/${PATH}`;
-        const authorization = ACCESS_TOKEN.access_token;
-        const token_type = ACCESS_TOKEN.token_type;
-        const headers = {
-            "Content-Type": "application/json",
-            authorization: `${token_type} ${authorization}`,
-            appKey: key,
-            appSecret: secret,
-            tr_id: "FHKST01010100",
-        };
+  try {
+    const ACCESS_TOKEN = await getToken();
+    const PATH = "uapi/domestic-stock/v1/quotations/inquire-price";
+    const URL = `${URL_BASE}/${PATH}`;
+    const authorization = ACCESS_TOKEN.access_token;
+    const token_type = ACCESS_TOKEN.token_type;
+    const headers = {
+      "Content-Type": "application/json",
+      authorization: `${token_type} ${authorization}`,
+      appKey: key,
+      appSecret: secret,
+      tr_id: "FHKST01010100",
+    };
 
-        const params = {
-            fid_cond_mrkt_div_code: "J",
-            fid_input_iscd: stockId,
-        };
-        const response = await axios.get(URL, { headers, params });
-        return res.json(response.data); 
-    } catch (error) {
-        console.error("Error:", error.message);
-        return res.status(500).json({
-            error_description: 'Failed to fetch current price',
-            error_code: 'SERVER_ERROR'
-        });
-    }
+    const params = {
+      fid_cond_mrkt_div_code: "J",
+      fid_input_iscd: stockId,
+    };
+    const response = await axios.get(URL, { headers, params });
+    return res.json(response.data.output.stck_prpr);
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).json({
+      error_description: "Failed to fetch current price",
+      error_code: "SERVER_ERROR",
+    });
+  }
 });
 
+app.post("/currentprice/overseas-stock/current-price", async (req, res) => {
+  const { stockId } = req.body;
 
-app.post('/api/overseas-stock/current-price', async (req, res) => {
-    const { stockId } = req.body; 
+  if (!stockId) {
+    return res.status(400).json({ error: "stockId is required" });
+  }
 
-    if (!stockId) {
-        return res.status(400).json({ error: "stockId is required" });
-    }
+  try {
+    const ACCESS_TOKEN = await getToken();
+    const PATH = "/uapi/overseas-price/v1/quotations/price";
+    const URL = `${URL_BASE}/${PATH}`;
+    const authorization = ACCESS_TOKEN.access_token;
+    const token_type = ACCESS_TOKEN.token_type;
+    const headers = {
+      authorization: `${token_type} ${authorization}`,
+      appKey: key,
+      appSecret: secret,
+      tr_id: "HHDFS00000300",
+    };
 
-    try {
-        const ACCESS_TOKEN = await getToken();
-        const PATH = "/uapi/overseas-price/v1/quotations/price";
-        const URL = `${URL_BASE}/${PATH}`;
-        const authorization = ACCESS_TOKEN.access_token;
-        const token_type = ACCESS_TOKEN.token_type;
-        const headers = {
-            authorization: `${token_type} ${authorization}`,
-            appKey: key,
-            appSecret: secret,
-            tr_id: "HHDFS00000300",
-        };
-
-        const params = {
-            AUTH: "",
-            EXCD: stockId.slice(0,3),
-            SYMB: stockId.slice(3)
-        };
-        const response = await axios.get(URL, { headers, params });
-        return res.json(response.data); 
-    } catch (error) {
-        console.error("Error:", error.message);
-        return res.status(500).json({
-            error_description: 'Failed to fetch current price',
-            error_code: 'SERVER_ERROR'
-        });
-    }
+    const params = {
+      AUTH: "",
+      EXCD: stockId.slice(0, 3),
+      SYMB: stockId.slice(3),
+    };
+    const response = await axios.get(URL, { headers, params });
+    return res.json(response.data.output.last);
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).json({
+      error_description: "Failed to fetch current price",
+      error_code: "SERVER_ERROR",
+    });
+  }
 });
 
+app.post("/currentprice/bond/current-price", async (req, res) => {
+  const { stockId } = req.body;
 
+  if (!stockId) {
+    return res.status(400).json({ error: "stockId is required" });
+  }
 
+  try {
+    const ACCESS_TOKEN = await getToken();
+    const PATH = "/uapi/domestic-bond/v1/quotations/inquire-price";
+    const URL = `${URL_BASE}/${PATH}`;
+    const authorization = ACCESS_TOKEN.access_token;
+    const token_type = ACCESS_TOKEN.token_type;
+    const headers = {
+      "Content-Type": "application/json",
+      authorization: `${token_type} ${authorization}`,
+      appKey: key,
+      appSecret: secret,
+      tr_id: "FHKBJ773400C0",
+      custtype: "P",
+    };
 
-
-app.post('/api/bond/current-price', async (req, res) => {
-    const { stockId } = req.body; 
-
-    if (!stockId) {
-        return res.status(400).json({ error: "stockId is required" });
-    }
-
-    try {
-        const ACCESS_TOKEN = await getToken();
-        const PATH = "/uapi/domestic-bond/v1/quotations/inquire-price";
-        const URL = `${URL_BASE}/${PATH}`;
-        const authorization = ACCESS_TOKEN.access_token;
-        const token_type = ACCESS_TOKEN.token_type;
-        const headers = {
-            "Content-Type": "application/json",
-            authorization: `${token_type} ${authorization}`,
-            appKey: key,
-            appSecret: secret,
-            tr_id: "FHKBJ773400C0",
-            custtype: "P"
-        };
-
-        const params = {
-            fid_cond_mrkt_div_code: "B",
-            fid_input_iscd: stockId,
-        };
-        const response = await axios.get(URL, { headers, params });
-        return res.json(response.data); 
-    } catch (error) {
-        console.error("Error:", error.message);
-        return res.status(500).json({
-            error_description: 'Failed to fetch current price',
-            error_code: 'SERVER_ERROR'
-        });
-    }
+    const params = {
+      fid_cond_mrkt_div_code: "B",
+      fid_input_iscd: stockId,
+    };
+    const response = await axios.get(URL, { headers, params });
+    return res.json(response.data.output.bond_prpr);
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).json({
+      error_description: "Failed to fetch current price",
+      error_code: "SERVER_ERROR",
+    });
+  }
 });
 
+app.post("/currentprice/ETF/current-price", async (req, res) => {
+  const { stockId } = req.body;
 
+  if (!stockId) {
+    return res.status(400).json({ error: "stockId is required" });
+  }
+  const url = `https://polling.finance.naver.com/api/realtime/domestic/stock/${stockId}`;
 
-app.post('/api/ETF/current-price', async (req, res) => {
-    const { stockId } = req.body; 
-    
-    if (!stockId) {
-        return res.status(400).json({ error: "stockId is required" });
-    }
-    const url = `https://polling.finance.naver.com/api/realtime/domestic/stock/${stockId}`;
-
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-        console.log('Stock Price Data:', data.datas[0]);
-        return res.json({ currentPrice: data.datas[0].closePrice}) ;
-    }catch (error) {
-        console.error('Error fetching stock price:', error.message);
-    }
-
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log("Stock Price Data:", data.datas[0]);
+    return res.json(data.datas[0].closePrice);
+  } catch (error) {
+    console.error("Error fetching stock price:", error.message);
+  }
 });
-
 
 app.listen(port, () => {
-    console.log(`REST API server is listening on port ${port}`);
+  console.log(`REST API server is listening on port ${port}`);
 });
